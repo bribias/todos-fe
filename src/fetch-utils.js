78b7de32dffcc4dd1f4abb4bfb2eb095efccf982
Brianna Bias <brianna.bias@gmail.com>
2021-06-30 23:31:56 -0700

    fetch-utils finish

--- src/fetch-utils.js
+++ src/fetch-utils.js
@@ -3,18 +3,46 @@ import request from 'superagent';
 
 const URL = 'https://vast-badlands-41470.herokuapp.com';
 
-export async function signup() {
-
+export async function signup(email, password) {
+    const data = await request
+        .post(`${URL}/auth/signup`)
+        .send({
+            email: email,
+            password: password,
+        })
+    return data.body.token;
 }
 
-export async function login() {
-
+export async function login(email, password) {
+    const data = await request
+        .post(`${URL}/auth/signin`)
+        .send({
+            email: email,
+            password: password,
+        })
+    return data.body.token;
 }
 
-export async function addTodos() {
-
+export async function getTodos(token) {
+    const data = await request
+        .get(`${URL}/api/todos`)
+        .set('Authorization', token)
+    return data.body;
 }
 
-export async function completeTodo() {
-    
+export async function addTodos(todo, token) {
+    const data = await request
+        .post(`${URL}/api/todos`)
+        .send({
+            todo: todo,
+            completed: false,
+        })
+    return data.body;
+}
+
+export async function completeTodo(id, token) {
+    const data = await request
+        .put(`${URL}/api/todos/${id}`)
+        .set('Authorization', token)
+    return data.body;
 }
