7575f819c86aeef367a070057d9158dc1daa5a9e
Brianna Bias <brianna.bias@gmail.com>
2021-06-30 18:00:46 -0700

    set up full wireframe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  Redirect
+} from 'react-router-dom';
+import HomePage from './HomePage';
+import LoginPage from './LoginPage';
+import SignupPage from './SignupPage';
+import TodosPage from './TodosPage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
 
-export default App;
+  }
+
+  longin = () => {
+
+  }
+
+  logout = () => {
+
+  }
+
+  render() {
+    return ()
+  }
+}
