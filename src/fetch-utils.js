78ed76f62a483fe26bcf23c35f80a7f4c3d9ccef
Brianna Bias <brianna.bias@gmail.com>
2021-06-30 17:47:08 -0700

    fetch-utils wireframe

---
+++ src/fetch-utils.js
@@ -0,0 +1,20 @@
+import { func } from 'prop-types';
+import request from 'superagent';
+
+const URL = 'https://vast-badlands-41470.herokuapp.com';
+
+export async function signup() {
+
+}
+
+export async function login() {
+
+}
+
+export async function addTodos() {
+
+}
+
+export async function completeTodo() {
+    
+}
